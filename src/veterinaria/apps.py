671fa36c1033b8b84faf789f4aa371378e2d6fc7
SAguirreT <sheila.aguirre@tecsup.edu.pe>
2026-09-12 17:57:36 -0500

    Delete src/veterinaria directory

--- src/veterinaria/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class VeterinariaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'veterinaria'
--- src/veterinaria/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Propietario
-
-
-class PropietarioForm(forms.ModelForm):
-    class Meta:
-        model = Propietario
-        fields = ['nombre', 'apellido', 'telefono', 'correo']
